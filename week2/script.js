4d78ec94f0214af13aea9a7889431a1b570c7b1b
kmu-jinwoon05 <jinwonzang5@gmail.com>
2026-04-04 23:22:38 +0900

    week2

--- week2/script.js
+++ week2/script.js
@@ -2,5 +2,5 @@ const cheerButton = document.getElementById('cheerButton');
 
 cheerButton.addEventListener('click', () => {
   cheerButton.textContent = '박진원의 학교 생활 살아남기';
-  alert('그런게 어딨냐');
+  alert('그런게 어딨냐.');
 });
